f674af82d3e7893dcf670a17a4d707fb6acc5522
Amin Muhana <amin.muhana@gmail.com>
2020-08-04 18:11:08 -0500

    Updates

---
+++ precache-manifest.72094e675fa4df3ee940ecc078deb464.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "837832439c67546aff9f7f7f8d347f9b",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "043f2aa76299efe492dd",
+    "url": "/monsters-rolodex/static/css/main.466b2878.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "043f2aa76299efe492dd",
+    "url": "/monsters-rolodex/static/js/main.8a47e2df.chunk.js"
+  },
+  {
+    "revision": "9b59177dc23ec2a3d1f7",
+    "url": "/monsters-rolodex/static/js/runtime-main.fa9629a0.js"
+  }
+]);
